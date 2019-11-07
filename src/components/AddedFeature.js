623021af0968f7ef6c3b0cc12f0dd121a3dce3f7
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-11-06 19:05:01 -0500

    stretch and mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Button } from 'react-bulma-components'
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <Button color="danger" size="large" onClick={() => props.removeFeature(props.feature)}>X</Button>
       {props.feature.name}
     </li>
   );
